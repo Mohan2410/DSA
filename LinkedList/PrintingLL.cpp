12c6f174d7ebfe1cbf8eb66fd7d67b314066ba73
Mohan Gawande <mohangawande93@gmail.com>
2025-08-19 23:02:08 +0530

    adding the PrintingLL.cpp file in LL folder

---
+++ LinkedList/PrintingLL.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    
+        int data;
+        Node* next;
+
+        Node(int data){
+            this->data = data;
+            this->next = NULL;
+        }
+};
+
+void printLL(Node* head){
+    Node* temp = head;
+
+    while(temp != NULL){
+        cout<<temp->data<<" -> ";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+int main(){
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+    Node* fourth = new Node(40);
+
+    // linking node 
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+
+    printLL(head);
+}
