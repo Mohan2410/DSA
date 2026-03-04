4ee281fca6077a45566fdd52c2864b705ebcdf6f
Mohan Gawande <mohangawande93@gmail.com>
2026-03-04 16:50:04 +0530

    two sum

---
+++ SumOfArray.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int arraySum(int arr[], int n) {
+
+    int sum = 0;
+
+    for(int i = 0; i < n; i++) {
+        sum = sum + arr[i];
+    }
+
+    return sum;
+}
+
+int main() {
+
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int result = arraySum(arr, n);
+
+    cout << "Sum of array = " << result;
+
+    return 0;
+}
